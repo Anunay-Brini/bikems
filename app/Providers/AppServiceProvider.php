165b134a2210d37ca2e0c6408f0ad716ce72b5b1
Anunay Brini <anunaybrini@gmail.com>
2026-03-25 22:01:37 +0530

    Auto-create default admin user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        try {
+            if (\Illuminate\Support\Facades\Schema::hasTable('users')) {
+                \App\Models\User::firstOrCreate(
+                    ['email' => 'admin@bikems.com'],
+                    [
+                        'name' => 'Admin',
+                        'password' => \Illuminate\Support\Facades\Hash::make('password123'),
+                        'role' => 'admin',
+                    ]
+                );
+            }
+        } catch (\Exception $e) {
+            // Ignore exception if the database is not yet ready or migrating
+        }
     }
 }
